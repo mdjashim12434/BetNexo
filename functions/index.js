e37d1eb6c900b554ea192dbf788481f40bf758fe
Md Jashim MIAH <mdjashimmiah12434@gmail.com>
2025-06-25 02:43:43 +0000

    this problem

--- functions/index.js
+++ functions/index.js
@@ -5,28 +5,32 @@ const API_KEY = "5FRoem2oLHlu1xmtF0IdRPTwdm37Znh60OjaNrl29MgRO1NVU6yyONOA8jbH";
 
 exports.getMatchOdds = functions.https.onRequest(async (req, res) => {
   // Allow CORS for all origins
-  res.set('Access-Control-Allow-Origin', '*');
+  res.set("Access-Control-Allow-Origin", "*");
 
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     // Send response to preflight OPTIONS requests
-    res.set('Access-Control-Allow-Methods', 'GET');
-    res.set('Access-Control-Allow-Headers', 'Content-Type');
-    res.set('Access-Control-Max-Age', '3600');
-    res.status(204).send('');
+    res.set("Access-Control-Allow-Methods", "GET");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-control-max-age", "3600");
+    res.status(204).send("");
     return;
   }
-  
+
   const fixtureId = req.query.id;
 
   if (!fixtureId) {
-    res.status(400).send("Fixture ID is required. Please provide it as a query parameter (e.g., ?id=YOUR_FIXTURE_ID).");
+    res.status(400).send(
+        "Fixture ID is required. " +
+        "Please provide it as a query parameter (e.g., ?id=YOUR_FIXTURE_ID).",
+    );
     return;
   }
 
   try {
-    const response = await axios.get(
-      `https://football.sportsmonks.com/api/v2.0/odds/fixture/${fixtureId}?api_token=${API_KEY}`
-    );
+    const url =
+      `https://football.sportsmonks.com/api/v2.0/odds/fixture/` +
+      `${fixtureId}?api_token=${API_KEY}`;
+    const response = await axios.get(url);
     res.status(200).json(response.data);
   } catch (err) {
     console.error("Odds fetch error:", err.message);
