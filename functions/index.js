36369a172ec18f0ea5255585c6a14365c92c5fd4
Md Jashim MIAH <mdjashimmiah12434@gmail.com>
2025-06-25 01:09:21 +0000

    সমস্যা: Firebase Functions ডেপ্লয় করলেও Console-এ কোনো Function দেখা যাচ

--- functions/index.js
+++ functions/index.js
@@ -23,8 +23,8 @@ setGlobalOptions({maxInstances: 10});
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
+const functions = require("firebase-functions");
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  res.send("Hello from Firebase!");
+});
